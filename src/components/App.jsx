962c0e86f779263cb06a4f4afcafb20baf15497f
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-11-04 09:19:59 +0200

    started making comonent Movies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
-import { Home } from '../components/Home/Home';
+import { Home } from '../pages/Home/Home';
+import { Movies } from '../pages/Movies/Movies';
 
 export const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="movies" element={< Movies />} />
+        
       </Routes>
     </>
   );
