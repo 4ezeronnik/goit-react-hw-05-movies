6e9d04f13bdb9065e19e506f3d1f0906a853ea26
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-11-04 21:10:37 +0200

    added comnponents and pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from '../pages/Home/Home';
 import { Movies } from '../pages/Movies/Movies';
+import { MovieDetails } from '../pages/MovieDetails/MovieDetails';
+import { Cast } from '../components/Cast/Cast';
+import { Reviews } from './Reviews/Reviews';
+
 
 export const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="movies" element={< Movies />} />
-        
+        <Route path="/movies" element={< Movies />} />
+          <Route path="/movies/:movieId" element={< MovieDetails />} />
+        <Route path="/movies/:movieId/cast" element={<Cast />} />
+        <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+          
       </Routes>
     </>
   );
