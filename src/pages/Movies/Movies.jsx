1982932fe06e7a6f0a7beb8c8e646eaf7f310fea
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-11-07 22:47:21 +0200

    added changes

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,9 +1,25 @@
+import { useMemo, useEffect, useState } from 'react';
+import { useLocation, useSearchParams } from 'react-router-dom';
 
 
 export const Movies = () => {
+    const location = useLocation();
+    const [movies, setMovies] = useState([]);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const filterParam = searchParams.get('filter') ?? '';
+
+    
+
+
     return (
         <>
-            Movies
+           <div>
+      <input
+        type="text"
+        value={value}
+        onChange={e => onChange(e.target.value)}
+      />
+    </div>
         </>
     );
 };
