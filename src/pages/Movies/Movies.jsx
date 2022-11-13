36730830c14393f12bd411a4daa0e427ea0b3961
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-11-13 23:38:15 +0200

    added changes

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Link, useLocation, useSearchParams } from 'react-router-dom';
 import { fetchSearchMovies } from 'services/api'
 
