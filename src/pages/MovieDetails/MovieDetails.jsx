35774a9b251945c342f5a2d485a8d1eafb9221fc
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-11-19 14:42:42 +0200

    added changes

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,4 +1,16 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchMovieDetails } from 'services/api';
+
 export const MovieDetails = () => {
+    const { movieId } = useParams();
+    const { movie, setMovie } = useState(null);
+
+    useEffect(() => {
+        fetchMovieDetails(movieId);
+    }, [movieId]);
+
+
     return (
         <>
             MovieDetails
