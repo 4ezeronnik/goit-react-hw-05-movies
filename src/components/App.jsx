4ceaa2b5f48f121cf9cd39ba67ee29f5fa72d333
Andrii Reznichenko <4ezeronnik@gmail.com>
2023-01-09 10:33:57 +0200

    added lazy and Suspense

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
 import { Route, Routes } from 'react-router-dom';
-import { Home } from '../pages/Home/Home';
-import { Movies } from '../pages/Movies/Movies';
-import { MovieDetails } from '../pages/MovieDetails/MovieDetails';
-import { Cast } from '../components/Cast/Cast';
-import { Reviews } from './Reviews/Reviews';
-import { NotFound } from './NotFound/NotFound';
-import { SharedLayout } from './SharedLayout/SharedLayout';
+// import { Home } from '../pages/Home/Home';
+// import { Movies } from '../pages/Movies/Movies';
+// import { MovieDetails } from '../pages/MovieDetails/MovieDetails';
+// import { Cast } from '../components/Cast/Cast';
+// import { Reviews } from './Reviews/Reviews';
+// import { NotFound } from './NotFound/NotFound';
+// import { SharedLayout } from './SharedLayout/SharedLayout';
+import { lazy, Suspense } from 'react';
+const Home = lazy(() => import('../pages/Home/Home'));
+const Movies = lazy(() => import('../pages/Movies/Movies'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('../components/Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
+const NotFound = lazy(() => import('./NotFound/NotFound'));
+const SharedLayout = lazy(() => import('./SharedLayout/SharedLayout'));
 
 
 export const App = () => {
   return (
     <>
+      <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<SharedLayout />} >
           <Route index element={<Home />} />
@@ -22,7 +31,8 @@ export const App = () => {
         </Route>
           <Route path="*" element={<NotFound />} />
           </Route>
-      </Routes>
+        </Routes>
+        </Suspense>
     </>
   );
 };
