937bfa20b3318cf14ba58b4bfd1b49ac2687926d
Andrii Reznichenko <4ezeronnik@gmail.com>
2023-01-14 23:49:16 +0200

    added changes

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -14,31 +14,31 @@ const MovieDetails = () => {
 
     const { title, vote_average, overview, genres, poster_path } = movies;
     const imageURL = 'https://image.tmdb.org/t/p/w500/';
-    
 
     useEffect(() => {
         if (!movieId) return;
-        fetchMovieDetails(movieId).then(setMovies);
+        fetchMovieDetails(movieId).then(setMovies, setStatus('resolved')).catch(console.error);     
     }, [movieId]);
     
     return (
         <>
-            <div>
-                
+            
+            {status === 'pending' && (<div>Loading</div>)}
+            {status === 'resolved' && (<div>
                 <Link to={backLinkHref}>
-                    <ArrowLeft size={20}/>
+                    <ArrowLeft size={20} />
                     Go back
                 </Link>
                 <h2>{title}</h2>
-                <img src={poster_path ? (`${imageURL}${poster_path}`) : pendingImage} alt={title}/>
-                <p>User Score: { Math.round(vote_average * 10) }% </p>
+                <img src={poster_path ? (`${imageURL}${poster_path}`) : pendingImage} alt={title} width="100px" height="150px"/>
+                <p>User Score: {Math.round(vote_average * 10)}% </p>
                 <h3>Overview</h3>
                 <p>{overview}</p>
                 <h3>Genres</h3>
                 {genres && (genres.map(genre => (
                     <li key={genre.id}>
-                  {genre.name}
-              </li>
+                        {genre.name}
+                    </li>
                 )))}
 
                 <ul>
@@ -46,13 +46,13 @@ const MovieDetails = () => {
                     <li>
                         <Link to="cast">Cast</Link>
                     </li>
-                      <li>
+                    <li>
                         <Link to="reviews">Reviews</Link>
                     </li>
                 </ul>
-                <Outlet />
 
-            </div>
+                <Outlet />
+            </div>) }
         </>
     );
 };
