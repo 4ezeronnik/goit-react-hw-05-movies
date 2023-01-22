77590fb5288d68421781bf00c0a35a8c11603c20
Andrii Reznichenko <4ezeronnik@gmail.com>
2023-01-22 23:29:09 +0200

    added state machine to Reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,30 +1,45 @@
 import { fetchMovieReviews } from "services/api";
 import { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
+import STATUS from "services/status-state-machine";
+import { ClipLoader } from "react-spinners";
+import NotFound from "components/NotFound/NotFound";
 
 const Reviews = () => {
     const [reviews, setReviews] = useState([]);
+    const [status, setStatus] = useState(STATUS.IDLE);
     const { movieId } = useParams();
     
     useEffect(() => {
-        fetchMovieReviews(movieId).then(setReviews);
+        if (!movieId) return;
+        setStatus(STATUS.PENDING);
+              const fetchFilmDetails = async () => {
+            try {
+                const fetchFilms = await fetchMovieReviews(movieId);
+                setReviews(fetchFilms);
+                setStatus(STATUS.RESOLVED);
+            } catch {
+                setStatus(STATUS.REJECTED)
+            };
+        };
+        fetchFilmDetails();
+       
     }, [movieId]);
     
 
     return (
 
         <>
-            { reviews.length > 0 ? (reviews.map(review => 
+            {status === STATUS.PENDING && <ClipLoader />}
+            {status === STATUS.RESOLVED && (reviews.length > 0 ? (reviews.map(review => 
                 <li key={review.id}>
                     <p>{review.author}</p>
                     <p>{review.content}</p>
                     
                 </li>))
                 :
-                (<p>We don't have any reviews for this movie.</p>)
-            }
-
-
+                (<p>We don't have any reviews for this movie.</p>))}
+            {status === STATUS.REJECTED && <NotFound/>}
         </>
     );
 };
